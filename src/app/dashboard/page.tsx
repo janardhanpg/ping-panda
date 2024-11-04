d266ef139396472953815e6c4c15e396655fd5f3
Janardhan <janardhanpg12@gmail.com>
2024-11-04 13:59:36 +0530

    Add show event and delete event

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import DashboardPage from "@/components/DashboardPage"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import React from "react"
+import DashboardPageContent from "./dashboard-page-content"
 
-const Page = () => {
-  return (
-    <div>
-      youre in dashboard
-    </div>
-  )
+const Page = async () => {
+  const auth = await currentUser()
+  if (!auth) {
+    redirect("/sign-in")
+  }
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+  if (!user) {
+    redirect("/sign-in")
+  }
+  return <DashboardPage title="Dashboard"><DashboardPageContent /></DashboardPage>
 }
 
 export default Page
